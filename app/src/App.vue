134d62ddb2a40de219df5eb48ff07350c5f19bea
Everton Cabral <evertoncab@gmail.com>
2021-11-22 15:38:57 -0300

    criação do header

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav-bar-component/>
+    <side-bar-component></side-bar-component>
+
+    <router-view/> 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBarComponent from './components/NavBarComponent'
+import SideBarComponent from './components/SideBarComponent.vue'
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    NavBarComponent,
+    SideBarComponent
+    
+    
   }
+  
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ app/src/components/SideBarComponent.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <b-sidebar
+      id="sidebar-no-header"
+      aria-labelledby="sidebar-no-header-title"
+      no-header
+      shadow
+      bg-variant="dark"
+      text-variant="light"
+      backdrop backdrop-varian="dark"
+    >
+      <template v-slot:default="{ hide }">
+        <div class="p-3">
+          <div class="d-flex align-items-center flex-column">
+          <b-avatar
+            variant="primary"
+            text="PS"
+            class="mb-3 mt-3"
+            size="5rem"
+          ></b-avatar>
+          
+            <h4 id="sidebar-no-header-title">
+             <br />
+              Rede de Relacionamento Cristã do Brasil
+            </h4>
+            <div class="divider-menu"></div>
+          </div>
+          <br />
+          <br />
+         
+          <p>SEJA BEM VINDO</p>
+           <br />
+          <nav class="mb-3">
+            <b-nav vertical>
+              <b-nav-item active @click="hide">
+                <b-icon icon="person"></b-icon> Usuários</b-nav-item
+              >
+              <b-nav-item href="#link-1" @click="hide">
+                <b-icon icon="emoji-sunglasses"></b-icon> Eventos</b-nav-item
+              >
+              <b-nav-item href="#link-2" @click="hide"
+                ><b-icon icon="book"></b-icon> Sobre Nós</b-nav-item
+              >
+            </b-nav>
+          </nav>
+          <b-button variant="primary" block @click="hide">FECHAR</b-button>
+        </div>
+      </template>
+    </b-sidebar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SideBarComponent",
+};
+</script >
+
+
+<style scoped>
+.divider-menu{
+  border:1px solid #fff;
+  width: 100%;
+
+}
+.nav-item > .nav-link {
+  color: #ffff;
+}
+
+.nav-item > .nav-link:hover {
+  color: #cccc;
+}
+</style>
