05f28a9071c2e857379a731de79222047dbfdc9c
Everton Cabral <evertoncab@gmail.com>
2021-11-23 21:39:57 -0300

    atualização

---
+++ app/src/views/Avatares.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="avatar">
+    <div class="mb-4 d-flex justify-contente-center mb-4">
+      <b-avatar> </b-avatar>
+       <b-avatar variant="primary" text="ES"> </b-avatar>
+       <b-avatar variant="success" icon="people-fill"> </b-avatar>
+
+       <b-avatar src="endereço da foto"> </b-avatar>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Avatar",
+  components: {
+
+  },
+};
+</script>
---
+++ app/src/views/Cadastro.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app" class="container mt-3">
+    <h3>Dados Pessoais</h3>
+    <hr />
+
+    <b-form>
+      <b-row>
+        <b-col md="6" sm="12">
+          <b-form-group label="Nome: " label-for="nome">
+            <b-form-input
+              v-model="form.nome"
+              id="nome"
+              placeholder="Nome Completo"
+            >
+            </b-form-input>
+          </b-form-group>
+        </b-col>
+        <b-col md="6" sm="12">
+          <b-form-group label="EMAIL: " label-for="email">
+            <b-form-input
+              v-model="form.email"
+              id="email"
+              placeholder="Digite com seu email"
+            >
+            </b-form-input>
+          </b-form-group>
+        </b-col>
+      </b-row>
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      form: {
+        nome,
+        email,
+      },
+    };
+  },
+};
+</script>
+
+<style>
+</style>
